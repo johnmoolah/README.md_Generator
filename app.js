ce68e4dbf9a4f5899d0839800a26a2148f5ff64d
John Delizo <johndelizo99@gmail.com>
2020-12-05 13:33:42 -0800

    fixed punctuation that broke code

--- app.js
+++ app.js
@@ -1,22 +1,50 @@
-const inquirer = require("inquirer");
+const inquirer = require(`inquirer`);
 
 inquirer
     .prompt([
         // ReadMe Questions
-        {type: "input", name: "TestInput", message: "enter test"},
-        {type: "input", name: "TestInput1", message: "enter test"},
-        {type: "input", name: "TestInput2", message: "enter test"},
-        {type: "input", name: "TestInput3", message: "enter test"},
-        ])
-        .then (({testInput,testInput3})) ; 
+        { type: "input", name: "TestInput", message: "enter a test" },
+        { type: "input", name: "TestInput1", message: "enter another test" },
+        { type: "input", name: "TestInput2", message: "enter test 3 "},
+        { type: "input", name: "TestInput3", message: "enter test 4 "},
+    ])
+    
+    .then((testRes) => {
+        console.log(testRes);
+    })
+    .catch((err) => console.log(err));
             
-    const str = "first test results: ${testInput}/nsecond test results: ${testInput3}";
+
+         (({testInput,testInput3})); 
+            const str = `first test results: ${testInput}\nsecond test results: ${testInput3}`;
+
+    const html = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>README_Generator</title>
+    </head>
+    <body>
+    <h1>${testInput}</h1>
+    <h1>${testInput3}</h1>
+
+    </body>
+    </html>
+    `;
+
+    fs.writeFile("test.html", html, (err) => {
+        if(err) throw err;
+        console.log("complete");
+        });
+    try{err} finally {console.log(err)};
         
         
 
-        /* // name
-        // city
-        // hobby
-        // food
-        // github username
-        // linkedIn URL */
+    //     /* // name
+    //     // city
+    //     // hobby
+    //     // food
+    //     // github username
+    //     // linkedIn URL */
