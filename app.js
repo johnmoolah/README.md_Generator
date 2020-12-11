bfccfe87ce51c440535e6942eac46b9b7dc210e0
John Delizo <johndelizo99@gmail.com>
2020-12-10 17:57:36 -0800

    Rearranged code

--- app.js
+++ app.js
@@ -4,16 +4,44 @@ const fs = require("fs")
 inquirer
     .prompt([
         /* Pass your questions in here */
-        { type: "input", name: "userInput1", message: "Title" },
-        { type: "input", name: "userInput2", message: "Table of Contents" },
-        { type: "input", name: "userInput3", message: "Description"},
-        { type: "input", name: "userInput4", message: "Usage"},
-        { type: "input", name: "userInput5", message: "Github"},
-        { type: "input", name: "userInput6", message: "LinkedIn"},
-        { type: "input", name: "userInput7", message: "License"},
+        { 
+        type: "input",
+        name: "What is the title of the project?",
+        message: "Title" 
+        },
+        { 
+        type: "input",
+        name: "userInput2", 
+        message: "Table of Contents" 
+        },
+        { 
+        type: "input",
+        name: "userInput3",
+        message: "Description"
+        },
+        {
+        type:"input",
+        name: "userInput4",
+        message: "Usage"
+        },
+        {
+        type: "input",
+        name: "userInput5", 
+        message: "Github"
+        },
+        {
+        type: "input",
+        name: "userInput6",
+        message: "LinkedIn"
+        },
+        {
+        type: "input", 
+        name: "userInput7", 
+        message: "License"},
     ])
     
-    .then(({userInput1, userInput2, userInput3, userInput4, userInput5, userInput6, userInput7}) => {
+    .then(({
+        userInput1, userInput2, userInput3, userInput4, userInput5, userInput6, userInput7}) => {
             const str = `Title: ${userInput1}\n Table of contents: ${userInput2}\n Descrption: ${userInput3}\n Usage: ${userInput4}\n Github:${userInput5}\n  LinkedIn:${userInput6}\n License:${userInput7}\n`
              
 
